e8bf10f9c3bad3b3d17e675e88a35bd24c40f944
Chris Gregory <cmgjr07@gmail.com>
2025-03-25 15:55:38 -0400

    Works. v1.0.0

--- dna-to-mrna.py
+++ dna-to-mrna.py
@@ -1,2 +1,26 @@
+DNA_to_RNA = {
+    "A": "U",
+    "T": "A",
+    "G": "C",
+    "C": "G",
+}
+
+def transcribe(dna):
+    mRNA = ""
+    for nucleotide in dna:
+        mRNA = mRNA + DNA_to_RNA[nucleotide]
+    return mRNA
+
+def codons(rna):
+    mRNA_codons = ""
+    for n in range(len(rna)):
+        if n % 3 == 0 and n != 0:
+            mRNA_codons = mRNA_codons + " "
+        mRNA_codons = mRNA_codons + rna[n]
+    return mRNA_codons
+
+def main():
+    print(f"mRNA sequence: {codons(transcribe(input("Enter DNA sequence: ")))}")
+
 if __name__ == "__main__":
-    print("Hello, world!")
+    main()
